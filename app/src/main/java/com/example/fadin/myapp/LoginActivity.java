f4294e22741d9c5604e317b05e335c5542096e2e
FADINGLIGHT <FadingLight9291117@users.noreply.github.com>
2018-10-16 13:37:38 +0800

    基本搭建完成

--- app/src/main/java/com/example/fadin/myapp/LoginActivity.java
+++ app/src/main/java/com/example/fadin/myapp/LoginActivity.java
@@ -89,6 +89,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
                 attemptLogin();
             }
         });
