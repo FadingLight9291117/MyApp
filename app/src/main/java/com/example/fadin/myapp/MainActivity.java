88fd8d43a2d25e928f8a3d3879e983a232a7849f
cosmopolitanXF <1411049809@qq.com>
2018-10-18 11:58:51 +0800

    xxxx

--- app/src/main/java/com/example/fadin/myapp/MainActivity.java
+++ app/src/main/java/com/example/fadin/myapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.fadin.myapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -14,9 +20,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private static final int PICK_CODE=0x110;   //请求码，随便设置的一个常量
+    private String mCurrentPhotoStr;
+    private ImageView mPhoto;
+    private Bitmap mPhotoImg;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,6 +60,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mPhoto=(ImageView)findViewById(R.id.imageView3);
+
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +82,63 @@ public class MainActivity extends AppCompatActivity
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        Button bt =(Button)findViewById(R.id.getImage);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,PICK_CODE);
+            }
+        });
+
     }
+
+    @Override          //接收返回的结果
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if(requestCode==PICK_CODE)
+        {
+
+            if(intent != null)
+            {
+                Uri uri= intent.getData();
+                Cursor cursor=getContentResolver().query(uri,null,null,null,null);
+                cursor.moveToFirst();
+                int idx =cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                mCurrentPhotoStr=cursor.getString(idx);     //获得图片的路径
+                cursor.close();
+                resizePhoto();  //图片压缩函数
+                mPhoto.setImageBitmap(mPhotoImg);
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+
+    //图片压缩函数实现 ,最后获得一个Bitmap
+    private void resizePhoto()
+    {
+        BitmapFactory.Options options =new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;  //不会去加载图片，只会去获取图片的尺寸
+
+        BitmapFactory.decodeFile(mCurrentPhotoStr,options);
+
+        double ratio = Math.max(options.outWidth*1.0d /1024f,options.outHeight*1.0d/1024f);//获取宽度和高度，并且让图片像素不超过1024,图片不能大于3M
+        //获取到一个合理尺寸的radio
+
+        options.inSampleSize=(int)Math.ceil(ratio);  //图片压缩成功
+
+        options.inJustDecodeBounds = false;
+
+        mPhotoImg=BitmapFactory.decodeFile(mCurrentPhotoStr,options);
+    }
+
+
 
     @Override
     public void onBackPressed() {
